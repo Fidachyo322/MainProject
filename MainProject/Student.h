5bc17c5eeba5a983d74addabc93df1f2101a8420
Fidachyo322 <baconvevo@mail.ru>
2025-06-13 19:12:25 +0300

    Add project files.

---
+++ MainProject/Student.h
@@ -0,0 +1,70 @@
+﻿#pragma once
+#include "main.h"
+
+class Student {
+public:
+	string firstname;
+	string surname;
+	int age;
+	int _class;
+	char gender;
+	bool alive;
+	double mark;
+	int size;
+	string* subjects;
+
+	
+	Student() : Student("no name", "no surname") {
+		//cout << "default-constructor" << endl;
+	}
+
+	Student(string firstname, string surname) : firstname(firstname),
+		surname(surname), age(0), _class(0), gender('m'), alive(false),
+		mark(0), size(0), subjects(nullptr) {
+		//cout << "constructor with arguments" << endl;	
+	}
+
+	Student(string firstname, string surname, int age) : Student(firstname,
+		surname, age, 0, 'm', true, 4, 0, nullptr) {
+		//cout << "constructor with arguments" << endl;
+	}
+
+	Student(string firstname, string surname, int age, int _class,
+		char gender, bool alive, double mark, int size, string* subjects) {
+		//cout << "canonical constructor" << endl;
+		this->firstname = firstname;
+		this->surname = surname;
+		this->age = age;
+		this->_class = _class;
+		this->gender = gender;
+		this->alive = alive;
+		this->mark = mark;
+		this->size = size;
+		this->subjects = subjects;
+	}
+
+	Student(const Student& student) : Student(student.firstname,
+		student.surname, student.age, student._class, student.gender,
+		student.alive, student.mark, student.size, student.subjects) {
+		//cout << "canonical constructor" << endl;
+	}
+
+	~Student() {
+		// cout << "destructor" << endl;
+		if (subjects != nullptr) {
+			delete[] subjects;
+		}
+	}
+
+	string toString() {
+		string s = firstname;
+		s += " " + surname + ".";
+		s += ", age = " + to_string(age);
+		s += ", class = " + to_string(_class);
+		s += ", gender = " + to_string(gender);
+		s += ", is alive - ";
+		s += (alive ? "yes" : "no");
+		s += ", average mark = " + to_string(mark);
+		return s;
+	}
+};
